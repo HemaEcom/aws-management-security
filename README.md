e540e9d19f87c316f182978260ff374beff17762
Serge Ivanov <sergey.ivanov@nordcloud.com>
2025-03-21 07:38:05 +0100

    updated checks

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# aws-management-security
-aws hema security scripts and files
+## Microsoft Defender InSpec Control
+
+The `check-msd.rb` script is an InSpec control that checks if Microsoft Defender is installed and running on both Linux and Windows platforms.
+
+### Details
+
+- **Control Name**: `microsoft_defender_running`
+- **Impact**: 1.0
+- **Title**: Check if Microsoft Defender is installed and running
+- **Description**: Ensures Microsoft Defender is installed and running on both Linux and Windows platforms.
+
+### Platform-Specific Checks
+
+- **Windows**:
+  - Checks if the `WinDefend` service is installed and running.
+  - Uses the `service` resource to verify the status of Microsoft Defender.
+
+- **Linux**:
+  - Checks if the `mdatp.service` is active and running.
+  - Uses the `command` resource to verify the status of Microsoft Defender.
+
+### Usage
+
+To run this control, execute the following command:
+
+```sh
+inspec exec check-msd.rb
+```
+
+Ensure that InSpec is installed and properly configured on your system before running the script.
--- check-msd.rb
+++ check-msd.rb
@@ -2,19 +2,25 @@
 
 control 'microsoft_defender_running' do
   impact 1.0
-  title 'Check if Microsoft Defender is installed and running'
-  desc 'Ensure Microsoft Defender is installed and running on both Linux and Windows platforms.'
-  
+  title 'Ensure Microsoft Defender is Installed and Running'
+  desc 'This control checks if Microsoft Defender is installed and running on both Linux and Windows platforms. It ensures that the security software is active and protecting the system.'
+
   # Check for Microsoft Defender on Windows
   if os.windows?
     describe service('WinDefend') do
       it { should be_installed }
       it { should be_running }
     end
+    describe command('Get-MpComputerStatus') do
+      its('stdout') { should match(/AMServiceEnabled\s+: True/) }
+    end
   # Check for Microsoft Defender on Linux
   elsif os.linux?
     describe command('systemctl status mdatp.service') do
       its('stdout') { should match(/active \(running\)/) }
+    end
+    describe command('mdatp health') do
+      its('stdout') { should match(/healthy/) }
     end
   end
 end
